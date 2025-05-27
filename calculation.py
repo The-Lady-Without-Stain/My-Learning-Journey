46bb10462170d4f4eb458bd570d3b676c88921c6
Basirah <basirahmusa12@gmail.com>
2025-05-27 14:18:25 +0100

    I entered the arithmetic Operators

--- calculation.py
+++ calculation.py
@@ -2,3 +2,16 @@ def myname():
     return 'basirah musa'
 
 print(f"my name is {myname()}")
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
