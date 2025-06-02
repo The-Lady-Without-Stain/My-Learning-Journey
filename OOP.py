9ddce816ce40f6f8ef28546e7fcad6a2df1f12d9
Basirah <basirahmusa12@gmail.com>
2025-06-02 23:21:25 +0100

    Object Oriented Programming

---
+++ OOP.py
@@ -0,0 +1,60 @@
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+c = Cat("Yasmin", 2)
+c2 = Cat("Ruby", 4)
+print(c.get_age())
+ 
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade 
+    
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+    
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+        return value / len(self.students)
+ 
+s1 = Student("Aminatu", 22, 86)
+s2 = Student("Sadiya", 23, 88)
+s3 = Student("Basirah", 24, 85)
+s4 = Student("Hanifa", 22, 85)
+s5 = Student("Shafi'i", 25, 89)
+
+course = Course("Pharmacology", 3)
+course.add_student(s1)
+course.add_student(s2)
+
+print(course.students[0].name)
+print (course.get_average_grade())
+ 
