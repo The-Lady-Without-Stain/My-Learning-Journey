e72baf69191f7f69fb25f1bc603a564b35d17c17
Basirah <basirahmusa12@gmail.com>
2025-06-18 22:14:31 +0100

    numpy

---
+++ Numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+print (np.__version__)
+
+arr = np.arange(13)
+print(type(arr))
+print (arr)
+      
+#checks the run time of your code
+
+Listt = list(range(1000001))
+for l in Listt:
+    Listt[l] * Listt [l]
+mult = Listt[l] * Listt [l]
+print(mult)
+
+varr = np.arange(1001)
+print(type(arr))
+varr = varr * varr
+print(varr)
